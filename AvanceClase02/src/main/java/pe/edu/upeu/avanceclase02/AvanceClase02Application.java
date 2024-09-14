add5296a9ceaf0d178f257905a89904e98f68e0d
YefriRonaldinhoLermaPacompia <ronallerma033@gmail.com>
2024-09-13 20:35:05 -0700

    DESARROLLO

---
+++ AvanceClase02/src/main/java/pe/edu/upeu/avanceclase02/AvanceClase02Application.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.avanceclase02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanceClase02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanceClase02Application.class, args);
+	}
+
+}
